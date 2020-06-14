9064810ce2484ecc5b2ef7268ebbea47ba3de3de
kaoriri <kaori.hiramatsu@gmail.com>
2020-06-15 08:17:53 +0900

    Creater MemoListScreen

--- App.js
+++ App.js
@@ -2,52 +2,26 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
-import MemoList from './src/components/MemoList';
-import CircleButton from './src/elements/CircleButton';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Appbar />
-        <MemoList />
-        <CircleButton>+</CircleButton>
+        <MemoListScreen />
       </View>
-
     );
   }
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
     backgroundColor: '#FFFDF6',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     paddingTop: 78,
   },
-
-    CircleButton: {
-      position: 'absolute',
-      bottom: 32,
-      right: 32,
-      width: 48,
-      height: 48,
-      backgroundColor: '#E31676',
-      borderRadius: 24,
-      justifyContent: 'center',
-      alignItems: 'center',
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.2,
-      shadowRadius: 3,
-    },
-    CircleButtonTitle: {
-      fontSize: 32,
-      lineHeight: 32,
-      color: '#fff',
-    },
-
 
 });
